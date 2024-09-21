9611856f8a4078d758184766105e1f831c3cb67f
Mingy123 <mingy123.coc@gmail.com>
2024-09-21 22:24:02 +0800

    fixed rendering code (wrong file)

--- generation/serve/draw.py
+++ generation/serve/draw.py
@@ -1,42 +1,73 @@
 import json
+import base64
+from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
 
 FONT_PATH = 'arial.ttf'
 
-# returns a PIL image
-def main(poster_path):
-    infile = open(poster_path, 'r')
-    template = json.load(infile)
-    infile.close()
+def find_max_font_size(text, width, height, draw):
+    font_size = 1
+    font = ImageFont.truetype(FONT_PATH, font_size)
+    while True:
+        lines = wrap_text(text, width, font, draw)
+        text_height = sum([font_size for line in lines])
+        if text_height > height or any(draw.textlength(line, font=font) > width for line in lines):
+            font_size -= 1
+            break
+        font_size += 1
+        font = ImageFont.truetype(FONT_PATH, font_size)
+    return ImageFont.truetype(FONT_PATH, font_size)
 
-    width, height = template['width'], template['height']
-    canvas = Image.new('RGB', (width, height), 'white')
-    draw = ImageDraw.Draw(canvas)
+def wrap_text(text, width, font, draw):
+    words = text.split()
+    lines = []
+    current_line = words[0]
+    for word in words[1:]:
+        test_line = f"{current_line} {word}"
+        if draw.textlength(test_line, font=font) <= width:
+            current_line = test_line
+        else:
+            lines.append(current_line)
+            current_line = word
+    lines.append(current_line)
+    return lines
 
-    def find_max_font(text, width, height, draw, font_path=FONT_PATH):
-        font_size = 1
-        font = ImageFont.truetype(font_path, font_size)
-        while True:
-            text_width = draw.textlength(text, font=font)
-            text_height = font_size
-            if text_width > width or text_height > height:
-                font_size -= 1
-                break
-            font_size += 1
-            font = ImageFont.truetype(font_path, font_size)
-        return font
+infile = open("final.json", 'r')
+template = json.load(infile)
+infile.close()
 
-    for item in template['elements']:
-        x0, y0 = item['x'], item['y']
-        x1, y1 = x0 + item['width'], y0 + item['height']
-        draw.rectangle([(x0, y0), (x1, y1)], outline="black", width=3)
+width, height = template['width'], template['height']
+canvas = Image.new('RGB', (width, height), 'white')
 
+draw = ImageDraw.Draw(canvas)
+
+for item in template['elements']:
+    x0, y0 = item['x'], item['y']
+    x1, y1 = x0 + item['width'], y0 + item['height']
+    draw.rectangle([(x0, y0), (x1, y1)], outline="black", width=3)
+
+    if item['contentType'] == 'text':
         text = item['content'] or item['name']
-        font = find_max_font(text, x1-x0, y1-y0, draw)
-        text_width = draw.textlength(text, font=font)
-        text_height = font.size
-        text_x = x0 + (x1 - x0 - text_width) / 2
-        text_y = y0 + (y1 - y0 - text_height) / 2
-        draw.text((text_x, text_y), text, fill="black", font=font)
+        rect_width = x1 - x0
+        rect_height = y1 - y0
+        font = find_max_font_size(text, rect_width, rect_height, draw)
+        lines = wrap_text(text, rect_width, font, draw)
+        total_text_height = sum([font.size for line in lines])
+        current_y = y0 + (rect_height - total_text_height) / 2
 
-    #canvas.save('out.png')
+        for line in lines:
+            text_width = draw.textlength(line, font=font)
+            text_x = x0 + (rect_width - text_width) / 2
+            draw.text((text_x, current_y), line, fill="black", font=font)
+            current_y += font.size
+    else:
+        assert item['contentType'] == 'image'
+        data = item['content'].removeprefix("data:image/jpeg;base64,")
+        print(data[:5], data[-5:])
+        image = Image.open(BytesIO(base64.b64decode( data )))
+        image = image.resize((item['width'], item['height']))
+        canvas.paste(image, (item['x'], item['y']))
+
+
+canvas.show()
+canvas.save('canvas.png')
